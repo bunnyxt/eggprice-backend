78672153e7e175fcea4e70093cf61b1a740dcf7e
bunnyxt <bunnyxt@outlook.com>
2019-04-29 23:00:33 +0800

    Create get location option api.

---
+++ api/get_location_option.php
@@ -0,0 +1,59 @@
+<?php
+
+require '../db.php';
+require '../models.php';
+
+header('Access-Control-Allow-Origin:*');
+
+$options = [];
+
+if ($provinces = $conn->query('select distinct province from location;')) {
+
+    // set province
+    while ($province = $provinces->fetch(PDO::FETCH_NUM)) {
+        $optionNode = new OptionNode();
+        $optionNode->value = $province[0];
+        $optionNode->label = $province[0];
+        $options[] = $optionNode;
+    }
+
+    // for each province
+    for ($i = 0; $i < count($options); $i++) {
+        
+        $provinceName = $options[$i]->value;
+        if ($cities = $conn->query('select distinct city from location where province="'.$provinceName.'";')) {
+
+            // set cities
+            while ($city = $cities->fetch(PDO::FETCH_NUM)) {
+                $optionNode = new OptionNode();
+                $optionNode->value = $city[0];
+                $optionNode->label = $city[0];
+                $options[$i]->children[] = $optionNode;
+            }
+
+            // for each city
+            for ($j = 0; $j < count($options[$i]->children); $j++) {
+
+                $cityName = $options[$i]->children[$j]->value;
+                if ($countries = $conn->query('select distinct country from location where province="'.$provinceName.'" && city="'.$cityName.'";')) {
+
+                    // set countries
+                    while ($country = $countries->fetch(PDO::FETCH_NUM)) {
+                        $optionNode = new OptionNode();
+                        $optionNode->value = $country[0];
+                        $optionNode->label = $country[0];
+                        $options[$i]->children[$j]->children[] = $optionNode;
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
+
+echo (json_encode($options, JSON_UNESCAPED_UNICODE));
+
+
+?>
